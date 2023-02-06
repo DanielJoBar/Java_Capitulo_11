e27a94b153424da9129cff01b687e0db0cbb1fee
DanielJoBar <115071984+DanielJoBar@users.noreply.github.com>
2023-02-06 09:13:57 +0100

    Add files via upload

---
+++ Ejercicio04/Ejercicio04.java
@@ -0,0 +1,41 @@
+package Ejercicio04;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Ejercicio04 {
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Error en los argumentos.");
+            System.out.println("Ayuda: ");
+            System.out.println("java Ejercicio4 file.txt");
+            return;
+        }
+        String file = args[0];
+        BufferedReader br = null;
+        ArrayList<String> palabras = new ArrayList<String>();
+        try{
+            br = new BufferedReader(new FileReader(file));
+            String linea = "";
+            while((linea =br.readLine() ) !=null){
+                palabras.add(linea);
+
+            }
+            br.close();
+            Collections.sort(palabras);
+            int pos =0;
+            String sortedFile = (pos=file.lastIndexOf(".txt"))!=-1?file.substring(0,pos):file+1+"_sorted.txt";
+            BufferedWriter bw = new BufferedWriter(new FileWriter(args[0]));
+            for(String l: palabras){
+                bw.write(l+"\n");
+            }
+            bw.close();
+        }
+        catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+    }
+}
